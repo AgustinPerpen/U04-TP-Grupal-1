0ac43bf9a282ba2e418368d5c8636d7923a54fc4
AgustinPerpen <agustinperpen@hotmail.com>
2021-11-30 17:05:08 -0300

    Correcciones TP

--- js/index.js
+++ js/index.js
@@ -1,75 +1,77 @@
+let zonas = document.getElementById("distritos");
+let cantidad = document.getElementById("cantidad");
+let ivaFinal;
+let zonasFinal;
+let resu = document.getElementById("result");
+
 function validarCantidadKwh() {
-      let cantidad = document.getElementById("cantidad");
-      if (cantidad.value <= 0) {
-            alert("El campo cantidad tiene que ser mayor a 0");
-            return false;
-      }
+  let cantidad = document.getElementById("cantidad");
+  if (cantidad.value <= 0) {
+    alert("El campo cantidad tiene que ser mayor a 0");
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function validarDistritos() {
-      let optForm = document.forms["frmCalc"]["distritos"].selectedIndex;
-      if (optForm == undefined || optForm == 0) {
-            alert("Debe seleccionar una opción en el campo distritos");
-            return false;
-      }
+  let optForm = document.forms["frmCalc"]["distritos"].selectedIndex;
+  if (optForm == undefined || optForm == 0) {
+    alert("Debe seleccionar una opción en el campo distritos");
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function validarCheckbox() {
-      if (document.getElementById('usuarios-prueba').checked) {
-            alert("Debe escoger un Tipo de Usuario");
-            return false;
-      }
+  if (document.getElementById("usuarios-prueba").checked) {
+    alert("Debe escoger un Tipo de Usuario");
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function calcularKwh() {
-      let iva = document.querySelector('input[name="usuarios"]:checked').value;
-      let zonas = document.getElementById("distritos").value;
-      let zonaCentro = document.getElementById("distrito-centro").value;
-      let zonaSur = document.getElementById("distrito-sur").value;
-      let zonaOeste = document.getElementById("distrito-oeste").value;
-      let zonaNorte = document.getElementById("distrito-norte").value;
-      let cantidad = document.getElementById("cantidad").value;
-      let resu = document.getElementById("result");
-      let ivaFinal;
-      let zonasFinal;
-      let flag = 0;
-      let flagIva = 0;
+  if (validarCheckbox()) {
+    if (validarCantidadKwh()) {
+      if (validarDistritos()) {
+        let flag = 0;
+        let iva = document.querySelector(
+          'input[name="usuarios"]:checked'
+        ).value;
+        if (iva == "residencial") {
+          ivaFinal = 0.21;
+        }
+        if (iva == "industrial") {
+          ivaFinal = 0.27;
+        }
 
-      if (iva == "residencial") {
-            ivaFinal = 0.21;
-            flagIva = 1;
+        if (zonas.value == "distrito-centro") {
+          zonasFinal = 5.8;
+          flag = 1;
+        }
+        if (zonas.value == "distrito-sur") {
+          zonasFinal = 5.4;
+          flag = 1;
+        }
+        if (zonas.value == "distrito-oeste") {
+          zonasFinal = 5.35;
+          flag = 1;
+        }
+        if (zonas.value == "distrito-norte") {
+          zonasFinal = 5.6;
+          flag = 1;
+        }
+
+        resultado = 102 + cantidad.value * zonasFinal * (1 + ivaFinal);
+        resultadoDecimal = resultado.toFixed(2);
+
+        if (flag == 1) {
+          resu.innerText = `$${resultadoDecimal}`;
+        }
       }
-
-      if (iva == "industrial") {
-            ivaFinal = 0.27;
-            flagIva = 1;
-      }
-
-      if (zonas == zonaCentro) {
-            zonasFinal = 5.80;
-            flag = 1;
-      }
-
-      if (zonas == zonaSur) {
-            zonasFinal = 5.40;
-            flag = 1;
-      }
-
-      if (zonas == zonaOeste) {
-            zonasFinal = 5.35;
-            flag = 1;
-      }
-
-      if (zonas == zonaNorte) {
-            zonasFinal = 5.60;
-            flag = 1;
-      }
-
-      resultado = 102 + (cantidad * zonasFinal) * (1 + ivaFinal);
-      resultadoDecimal = resultado.toFixed(2);
-
-      if (flag == 1 && flagIva == 1) {
-            resu.innerText = `$${resultadoDecimal}`;
-      }
-
+    }
+  }
 }
